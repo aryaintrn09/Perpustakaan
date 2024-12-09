dc33cf3eabeed43b57077611e8ed745193c41b87
Arya Ngurah Intaran <146977274+aryaintrn09@users.noreply.github.com>
2024-12-09 23:51:34 +0800

    layout

---
+++ Perpustakaan/Form1.Designer.cs
@@ -0,0 +1,140 @@
+﻿namespace Perpustakaan
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtJudul = new System.Windows.Forms.TextBox();
+            this.txtPenulis = new System.Windows.Forms.TextBox();
+            this.txtTahunTerbit = new System.Windows.Forms.TextBox();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.dataGridViewBuku = new System.Windows.Forms.DataGridView();
+            this.lblJudul = new System.Windows.Forms.Label();
+            this.lblPenulis = new System.Windows.Forms.Label();
+            this.lblTahunTerbit = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBuku)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtJudul
+            // 
+            this.txtJudul.Location = new System.Drawing.Point(142, 28);
+            this.txtJudul.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtJudul.Name = "txtJudul";
+            this.txtJudul.Size = new System.Drawing.Size(226, 20);
+            this.txtJudul.TabIndex = 0;
+            this.txtJudul.Enter += new System.EventHandler(this.txtJudul_Enter);
+            this.txtJudul.Leave += new System.EventHandler(this.txtJudul_Leave);
+            // 
+            // txtPenulis
+            // 
+            this.txtPenulis.Location = new System.Drawing.Point(142, 66);
+            this.txtPenulis.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtPenulis.Name = "txtPenulis";
+            this.txtPenulis.Size = new System.Drawing.Size(226, 20);
+            this.txtPenulis.TabIndex = 1;
+            this.txtPenulis.Enter += new System.EventHandler(this.txtPenulis_Enter);
+            this.txtPenulis.Leave += new System.EventHandler(this.txtPenulis_Leave);
+            // 
+            // txtTahunTerbit
+            // 
+            this.txtTahunTerbit.Location = new System.Drawing.Point(142, 103);
+            this.txtTahunTerbit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtTahunTerbit.Name = "txtTahunTerbit";
+            this.txtTahunTerbit.Size = new System.Drawing.Size(226, 20);
+            this.txtTahunTerbit.TabIndex = 2;
+            this.txtTahunTerbit.Enter += new System.EventHandler(this.txtTahunTerbit_Enter);
+            this.txtTahunTerbit.Leave += new System.EventHandler(this.txtTahunTerbit_Leave);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(212, 127);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 24);
+            this.btnAdd.TabIndex = 3;
+            this.btnAdd.Text = "Tambah Buku";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // dataGridViewBuku
+            // 
+            this.dataGridViewBuku.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewBuku.Location = new System.Drawing.Point(9, 162);
+            this.dataGridViewBuku.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.dataGridViewBuku.Name = "dataGridViewBuku";
+            this.dataGridViewBuku.Size = new System.Drawing.Size(477, 140);
+            this.dataGridViewBuku.TabIndex = 4;
+            // 
+            // lblJudul
+            // 
+            this.lblJudul.AutoSize = true;
+            this.lblJudul.Location = new System.Drawing.Point(142, 15);
+            this.lblJudul.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblJudul.Name = "lblJudul";
+            this.lblJudul.Size = new System.Drawing.Size(35, 13);
+            this.lblJudul.TabIndex = 5;
+            this.lblJudul.Text = "Judul:";
+            // 
+            // lblPenulis
+            // 
+            this.lblPenulis.AutoSize = true;
+            this.lblPenulis.Location = new System.Drawing.Point(142, 53);
+            this.lblPenulis.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblPenulis.Name = "lblPenulis";
+            this.lblPenulis.Size = new System.Drawing.Size(44, 13);
+            this.lblPenulis.TabIndex = 6;
+            this.lblPenulis.Text = "Penulis:";
+            // 
+            // lblTahunTerbit
+            // 
+            this.lblTahunTerbit.AutoSize = true;
+            this.lblTahunTerbit.Location = new System.Drawing.Point(142, 90);
+            this.lblTahunTerbit.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTahunTerbit.Name = "lblTahunTerbit";
+            this.lblTahunTerbit.Size = new System.Drawing.Size(71, 13);
+            this.lblTahunTerbit.TabIndex = 7;
+            this.lblTahunTerbit.Text = "Tahun Terbit:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 325);
+            this.Controls.Add(this.dataGridViewBuku);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.lblTahunTerbit);
+            this.Controls.Add(this.txtTahunTerbit);
+            this.Controls.Add(this.lblPenulis);
+            this.Controls.Add(this.txtPenulis);
+            this.Controls.Add(this.lblJudul);
+            this.Controls.Add(this.txtJudul);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Aplikasi Perpustakaan";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBuku)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.TextBox txtJudul;
+        private System.Windows.Forms.TextBox txtPenulis;
+        private System.Windows.Forms.TextBox txtTahunTerbit;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.DataGridView dataGridViewBuku;
+        private System.Windows.Forms.Label lblJudul;
+        private System.Windows.Forms.Label lblPenulis;
+        private System.Windows.Forms.Label lblTahunTerbit;
+    }
+}
---
+++ Perpustakaan/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Perpustakaan
+{
+    public partial class Form1 : Form
+    {
+        private string connectionString = "server=localhost;database=PerpustakaanDB;user=root;password=;";
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetPlaceholderText();
+            InitializeDataGridView();
+        }
+
+        private void SetPlaceholderText()
+        {
+            txtJudul.Text = "Judul Buku";
+            txtPenulis.Text = "Penulis";
+            txtTahunTerbit.Text = "Tahun Terbit";
+            txtTahunTerbit.ForeColor = System.Drawing.Color.Gray;
+            txtJudul.ForeColor = System.Drawing.Color.Gray;
+            txtPenulis.ForeColor = System.Drawing.Color.Gray;
+        }
+
+        private void InitializeDataGridView()
+        {
+            // Menambahkan kolom ke DataGridView
+            dataGridViewBuku.Columns.Add("Judul", "Judul Buku");
+            dataGridViewBuku.Columns.Add("Penulis", "Nama Penulis");
+            dataGridViewBuku.Columns.Add("TahunTerbit", "Tahun Terbit");
+        }
+
+        private void txtJudul_Enter(object sender, EventArgs e)
+        {
+            if (txtJudul.Text == "Judul Buku")
+            {
+                txtJudul.Text = "";
+                txtJudul.ForeColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void txtJudul_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtJudul.Text))
+            {
+                SetPlaceholderText();
+            }
+        }
+
+        private void txtPenulis_Enter(object sender, EventArgs e)
+        {
+            if (txtPenulis.Text == "Penulis")
+            {
+                txtPenulis.Text = "";
+                txtPenulis.ForeColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void txtPenulis_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPenulis.Text))
+            {
+                SetPlaceholderText();
+            }
+        }
+
+        private void txtTahunTerbit_Enter(object sender, EventArgs e)
+        {
+            if (txtTahunTerbit.Text == "Tahun Terbit")
+            {
+                txtTahunTerbit.Text = "";
+                txtTahunTerbit.ForeColor = System.Drawing.Color.Black;
+            }
+        }
+
+        private void txtTahunTerbit_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTahunTerbit.Text))
+            {
+                SetPlaceholderText();
+            }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            // Validasi input
+            if (string.IsNullOrWhiteSpace(txtJudul.Text) ||
+                string.IsNullOrWhiteSpace(txtPenulis.Text) ||
+                string.IsNullOrWhiteSpace(txtTahunTerbit.Text) ||
+                txtJudul.Text == "Judul Buku" ||
+                txtPenulis.Text == "Penulis" ||
+                txtTahunTerbit.Text == "Tahun Terbit")
+            {
+                MessageBox.Show("Semua kolom harus diisi.");
+                return;
+            }
+
+            int tahunTerbit;
+            if (!int.TryParse(txtTahunTerbit.Text, out tahunTerbit))
+            {
+                MessageBox.Show("Tahun terbit harus berupa angka yang valid.");
+                return;
+            }
+
+            // Jika semua validasi berhasil, simpan buku ke database
+            SaveBookToDatabase(txtJudul.Text, txtPenulis.Text, tahunTerbit);
+
+            // Tambahkan buku ke DataGridView
+            dataGridViewBuku.Rows.Add(txtJudul.Text, txtPenulis.Text, tahunTerbit);
+
+            // Kosongkan input setelah menambah
+            txtJudul.Clear();
+            txtPenulis.Clear();
+            txtTahunTerbit.Clear();
+            SetPlaceholderText();
+        }
+
+        private void SaveBookToDatabase(string judul, string penulis, int tahunTerbit)
+        {
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "INSERT INTO Buku (Judul, Penulis, TahunTerbit) VALUES (@Judul, @Penulis, @TahunTerbit)";
+                MySqlCommand cmd = new MySqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Judul", judul);
+                cmd.Parameters.AddWithValue("@Penulis", penulis);
+                cmd.Parameters.AddWithValue("@TahunTerbit", tahunTerbit);
+                cmd.ExecuteNonQuery();
+            }
+        }
+    }
+}
